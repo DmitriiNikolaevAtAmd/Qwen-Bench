0bdf2e3addae9b6bafe7ae1021a787ab4100fa22
Dmitrii Nikolaev <dmnikola@amd.com>
2026-02-12 22:06:23 +0200

    refactored

---
+++ scripts/run_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/platform.sh"
+
+run_docker \
+    --network=host \
+    --env-file config.env \
+    --env-file secrets.env \
+    "$IMAGE" bash scripts/data.sh "$@"
