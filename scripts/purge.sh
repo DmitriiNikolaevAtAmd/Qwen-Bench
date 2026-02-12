41e9517ee459c83802464855dd7c9f449b6aea9e
Dmitrii Nikolaev <dmnikola@amd.com>
2026-02-12 23:45:26 +0200

    refactored

--- scripts/purge.sh
+++ scripts/purge.sh
@@ -11,7 +11,7 @@ rm -rf "${OUTPUT_DIR:?}"/*
 mkdir -p "$OUTPUT_DIR"
 
 echo "Removing cache..."
-rm -rf "$HF_HOME"
+rm -rf "$HF_HOME" "$HF_DATASETS_CACHE"
 
 if [ "$1" = "--with-data" ]; then
     echo "Removing data..."
